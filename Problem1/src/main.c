e14e4cc7b9778c85052be3906d14e5527769e99c
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-11-20 11:06:39 +0100

    Solve problem 1 section 2

--- Problem1/src/main.c
+++ Problem1/src/main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 int get_number_of_bytes(char *filename);
+int printnbytes(char *filename);
+pid_t printnbytes_fork(char *filename);
+
 
 /** @brief Given a file path, returns the number of bytes in it.
  * @param filename A valid file path
@@ -16,31 +21,58 @@ int get_number_of_bytes(char *filename)
     if (fp == NULL)  return -1;
 
     int nchars = 0;
-    for(fgetc(fp); !feof(fp); fgetc(fp)) {
-        nchars++;
-    }
+    for(fgetc(fp); !feof(fp); fgetc(fp))  nchars++;
 
     fclose(fp);
 
     return nchars;
 }
 
+int printnbytes(char *filename)
+{
+    int nbytes = get_number_of_bytes(filename);
+    if (nbytes < 0) {
+        fprintf(stderr, "Error reading from file '%s'. Check existence and access permissions.\n", filename);
+        return 1;
+    }
+    printf("Number of bytes in file '%s': %d\n", filename, nbytes);
+    return 0;
+}
+
+pid_t printnbytes_fork(char *filename)
+{
+    pid_t pid = fork();
+
+    if (pid == -1) { // Error only
+        fprintf(stderr, "Error forking.\n");
+    }
+    if (pid == 0) { // Child only
+        printnbytes(filename);
+        exit(EXIT_SUCCESS);
+    }
+    
+    // Parent only
+    return pid;
+}
+
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "Wrong number of arguments: 1 expected, %d given\n", argc-1);
+    // Check min args
+    if (argc < 2) {
+        fprintf(stderr, "Wrong number of arguments: at least 1 expected, %d given\n", argc-1);
         exit(EXIT_FAILURE);
     }
 
-    char *filepath = argv[1];
-    int nbytes = get_number_of_bytes(filepath);
-    if (nbytes < 0) {
-        fprintf(stderr, "Error reading from file '%s'. Check existence and access permissions.\n", filepath);
-        exit(EXIT_FAILURE);
+    // Fork for each argument given
+    for (int i=0; i<argc-1; i++)  printnbytes_fork(argv[i+1]);
+    
+    // Wait for all processes
+    for (int i=0; i<argc-1; i++) {
+        int status;
+        pid_t pid = wait(&status);
+        if (status != EXIT_SUCCESS)  fprintf(stderr, "Process %d failed with status %d\n", pid, status);
     }
-
-    printf("Number of bytes in file '%s': %d\n", filepath, nbytes);
 
     exit(EXIT_SUCCESS);
 }
